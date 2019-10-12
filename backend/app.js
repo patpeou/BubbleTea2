e49fb36090250340f807617664c26ecb45a4dcd5
Sophat Peou <sophat.peou@gmail.com>
2019-10-12 15:46:57 -0700

    test test test

--- backend/app.js
+++ backend/app.js
@@ -33,6 +33,14 @@ mongoose.connect("mongodb+srv://sopa:Megaman69!@cluster0-spcvx.mongodb.net/test?
 
 // ------------------------------------ END MONGODB ----------------------------------------------------------
 
+app.post('/register', (req , res) => {
+  const newUser = new User();
+
+  newUser.email = req.body.email;
+  newUser.password = req.body.password;
+
+});
+
 app.post('/users', (req, res) => {
 
   const newUser  = new User({
@@ -55,9 +63,41 @@ app.get('/users', (req,res)=>{
   });
 });
 
+// app.patch('/users/:id',( req, res) => {
+//   const id = req.params.id;
+//   const firstName = req.body.firstName;
+//   const lastName = req.params.lastName;
+//
+//   User.findByIdAndUpdate(id, {$set: { firstName: firstName, lastName: lastName }}, {new: true} )
+//     .then(savedUser => {
+//       res.send('USER SAVED BY PATCH');
+//     });
+// });
 
+app.put('/users/:id',(req, res) => {
+  const id = req.params.id;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
 
+  User.findOne({ _id : id})
+  .then(user => {
+    user.firstName = req.body.firstName;
+    user.lastName = req.body.lastName;
 
+    user.save().then(userSaved => {
+      res.send(userSaved);
+    }).catch( err =>
+      console.log(err));
+  });
+});
+
+app.delete('/users/:id',(req, res) =>{
+  User.findByIdAndRemove({_id: req.params.id}).then( user => {
+    user.remove().then(userRemoved => {
+      res.send(`user ${userRemoved.firstName} removed`);
+    });
+  });
+});
 
 // app.get('/',(req, res) => {
 //
@@ -69,11 +109,8 @@ app.listen(4500, () => {
   console.log(`
     listening on port 4500
   `)
-
 });
-
 module.exports = app;
-
 
 // app.use((req, res, next) => {
 //   console.log('First middleware');
--- backend/models/User.js
+++ backend/models/User.js
@@ -2,6 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minLength: 3
+  },
+  password:{
+    type: String,
+    required: true,
+    minLength: 5
+  },
   firstName: {
     type: String,
     required: true,
