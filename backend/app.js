4c0b0ad3e085e385a9e99854ade4ba1751b57e41
Sophat Peou <sophat.peou@gmail.com>
2019-10-13 01:54:57 -0700

    test test test

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,16 @@ const mongoose = require('mongoose');
 const User = require('./models/User');
 const express = require('express');
 const app = express();
-
 const bodyParser = require('body-parser');
+
+const bcrypt = require('bcryptjs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+
+mongoose.Promise = global.Promise;
 
 app.use((req, res, next)=>{
   res.setHeader("Access-Control-Allow-Origin","*");
@@ -23,7 +26,7 @@ app.use((req, res, next)=>{
 
 // ------------------------------------MONGODB -------------------------------------------
 
-mongoose.connect("mongodb+srv://sopa:Megaman69!@cluster0-spcvx.mongodb.net/test?retryWrites=true&w=majority",
+mongoose.connect("mongodb+srv://sopa:Megaman69!@cluster0-spcvx.mongodb.net/login?retryWrites=true&w=majority",
   { useNewUrlParser: true, useUnifiedTopology: true })
   .then( () => {
     console.log('Connected to database!');
@@ -39,6 +42,33 @@ app.post('/register', (req , res) => {
   newUser.email = req.body.email;
   newUser.password = req.body.password;
 
+  bcrypt.genSalt(10, (err, salt)=> {
+    bcrypt.hash(newUser.password, salt, (err,hash)=>{
+
+      if(err) {return err}
+
+      newUser.password = hash;
+      newUser.save().then(userSaved => {
+        res.send('USER SAVED');
+      }).catch( err => {
+        res.send(err);
+      });
+    });
+  });
+});
+
+app.post('/login', (req, res)=>{
+  User.findOne({ email: req.body.email }).then(user => {
+    if(user){
+      bcrypt.compare(req.body.password, user.password, (err, matched) => {
+        if(matched){
+          res.send('USER LOGGED IN');
+        } else {
+          res.send('USER DIDN &#39;T LOG IN');
+        }
+      });
+    }
+  });
 });
 
 app.post('/users', (req, res) => {
@@ -54,7 +84,6 @@ app.post('/users', (req, res) => {
   }).catch(err=>{
     res.status(404).send('USER NOT SAVED');
   });
-
 });
 
 app.get('/users', (req,res)=>{
@@ -62,17 +91,6 @@ app.get('/users', (req,res)=>{
     res.status(200).send(users);
   });
 });
-
-// app.patch('/users/:id',( req, res) => {
-//   const id = req.params.id;
-//   const firstName = req.body.firstName;
-//   const lastName = req.params.lastName;
-//
-//   User.findByIdAndUpdate(id, {$set: { firstName: firstName, lastName: lastName }}, {new: true} )
-//     .then(savedUser => {
-//       res.send('USER SAVED BY PATCH');
-//     });
-// });
 
 app.put('/users/:id',(req, res) => {
   const id = req.params.id;
@@ -99,18 +117,32 @@ app.delete('/users/:id',(req, res) =>{
   });
 });
 
-// app.get('/',(req, res) => {
-//
-//   res.send('it works!');
-//
-// });
-
 app.listen(4500, () => {
   console.log(`
     listening on port 4500
   `)
 });
 module.exports = app;
+
+/////////// CODE WASTE ZONE ////////////////////////
+
+// app.get('/',(req, res) => {
+//
+//   res.send('it works!');
+//
+// });
+
+// app.patch('/users/:id',( req, res) => {
+//   const id = req.params.id;
+//   const firstName = req.body.firstName;
+//   const lastName = req.params.lastName;
+//
+//   User.findByIdAndUpdate(id, {$set: { firstName: firstName, lastName: lastName }}, {new: true} )
+//     .then(savedUser => {
+//       res.send('USER SAVED BY PATCH');
+//     });
+// });
+
 
 // app.use((req, res, next) => {
 //   console.log('First middleware');
--- backend/models/User.js
+++ backend/models/User.js
@@ -14,23 +14,23 @@ const UserSchema = new Schema({
     required: true,
     minLength: 5
   },
-  firstName: {
-    type: String,
-    required: true,
-    minLength: 4,
-    trim: true
-  },
-  lastName: {
-    type: String,
-    required: true,
-    minLength: 4,
-    trim: true
-  },
-  isActive: {
-    type: Number,
-    default: 0,
-    minLength: 4
-  }
+  // firstName: {
+  //   type: String,
+  //   required: true,
+  //   minLength: 4,
+  //   trim: true
+  // },
+  // lastName: {
+  //   type: String,
+  //   required: true,
+  //   minLength: 4,
+  //   trim: true
+  // },
+  // isActive: {
+  //   type: Number,
+  //   default: 0,
+  //   minLength: 4
+  // }
 });
 
 module.exports = mongoose.model('users', UserSchema);
